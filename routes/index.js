e04c095e58f883d8becdb483515ca9679f0c62fe
Masaki Pierce <m@Masakis-MacBook-Pro.local>
2022-01-11 18:16:32 -0500

    Adding practical content

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var https = require("https");
+var router = express.Router();
+var postalAbbreviations = require("../us_state.js");
+
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Find My Election",
+    states: postalAbbreviations,
+  });
+});
+
+function getOcdIds(city, stateAbbr) {
+  var stateId = stateAbbr.toLowerCase();
+  var cityId = city.toLowerCase().replace(" ", "_");
+  var stateOcdId = ocdIdStatePrefix + stateId;
+  var cityOcdId = stateOcdId + ocdIdPlacePrefix + cityId;
+  return [stateOcdId, cityOcdId];
+}
+
+router.post("/search", function (req, res, next) {
+  var { city, state } = req.body;
+  var ocdIds = getOcdIds(city, state);
+  console.log(ocdIds.join());
+  https.get(
+    "https://api.turbovote.org/elections/upcoming?district-divisions=" +
+      ocdIds.join(","),
+    { headers: { Accept: "application/json" } },
+    (result) => {
+      let electionStream = "";
+      result.on("data", (chunk) => {
+        electionStream += chunk;
+      });
+      result.on("end", () => {
+        res.render("search-results", {
+          title: "Search Results",
+          elections: JSON.parse(electionStream),
+        });
+      });
+    }
+  );
+});
+
+module.exports = router;
